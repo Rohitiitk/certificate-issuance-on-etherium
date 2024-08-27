943fa7e4e6432da4285cdb160f85c127c82b9a34
Rohit <rohitraj21@iitk.ac.in>
2024-08-27 23:18:42 +0530

    updated the code

--- hardhat/hardhat.config.cjs
+++ hardhat/hardhat.config.cjs
@@ -2,7 +2,16 @@ require("@nomicfoundation/hardhat-toolbox");
 require('@nomiclabs/hardhat-ethers');
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork:"localhost",
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 2
+      }
+    }
+  },
+  defaultNetwork:"hardhat",
   networks:{
     localhost: {
       url:"http://127.0.0.1:8545"
@@ -10,6 +19,6 @@ module.exports = {
     hardhat:{
       
     }
-  },
-  solidity: "0.8.24",
+  }
+  
 };
